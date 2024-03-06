0d3afb379723fab605489c794393f9c0c2f8fd3d
zhostetl <zhostetler193@gmail.com>
2024-03-06 07:50:39 -0500

    ridge regression for adjusted efficiencies; not quite complete

--- adjust_eff.py
+++ adjust_eff.py
@@ -14,30 +14,81 @@ file = r'C:\Users\zhostetl\Documents\11_CBB\99_git\NC2A-CBB\02_cleandata\cleaned
 
 df = pd.read_excel(file)
 
-print(df.head(15))
+# print(df.head(15))
+
+def add_opp(df):
+    games = df['GameID'].unique()
+    
+    for game in games: 
+        game_indices = df[df['GameID'] == game]
+        df.loc[game_indices.index[0], 'Opponent'] = df.loc[game_indices.index[1], 'Team']
+        df.loc[game_indices.index[1], 'Opponent'] = df.loc[game_indices.index[0], 'Team']
+    return df
 
 ## need to set it up as the team name in a column/variable; include the opponent in that same column and then home field advantage and the distance traveled 
 ## this is what gets used for the pd.get_dummies function
 ## then you can set the outcome variable to the desired stat
 ## this is the outcome variable that we want to adjust to 
+        
+df = add_opp(df)
 
-dummy_df = pd.get_dummies(df[['Team','Raw_Def_Eff']])
+# print(df[['Team','Opponent','GameID','Win','Loss']].head(15))
 
-print(dummy_df.head(15))
+dummy_df = pd.get_dummies(df[['Team','Opponent','Home']])
 
-alf = 175
-stat = 'Raw_Off_Eff'
+#stats to adjust for: 
+#Raw_Off_Eff, Raw_Def_Eff
+# + four factors
+# off_eFg, off_TOV, off_ORB, off_FTR
+# def_eFg, def_TOV, def_ORB, def_FTR
+stats_to_adjust = ['Raw_Off_Eff','Raw_Def_Eff','off_eFG','off_TOV','off_ORB','off_FTR','def_eFG','def_TOV','def_ORB','def_FTR']
+adjusted_dict = {}
+for stat in stats_to_adjust:
+    rdcv = linear_model.RidgeCV(alphas = [0.1,0.5,1,2,3,4,5,10,15], fit_intercept = True)
+    rdcv.fit(dummy_df,df[stat])
+    alf = rdcv.alpha_
+    r_squared = rdcv.score(dummy_df,df[stat])
+    print(f"\n\nridge regression r-squared: {r_squared:0.3f}, alpha: {alf} for {stat}\n\n")
+    reg = linear_model.Ridge(alpha = alf, fit_intercept = True)
+    reg.fit(X = dummy_df, y = df[stat])
+    dfRegResults = pd.DataFrame({
+        'coef_name': dummy_df.columns.values,
+        'ridge_reg_coef': reg.coef_})
+    
+    dfRegResults['ridge_reg_value'] = (dfRegResults['ridge_reg_coef']+reg.intercept_)
+    # print(dfRegResults.tail(20))
+    # sdf = dfRegResults[dfRegResults['coef_name'].str.contains('Baylor')].rename(columns = {"ridge_reg_value": stat}).reset_index(drop = True)
+    # print(sdf)
+    
+    adjusted_df = dfRegResults[dfRegResults['coef_name'].str.contains('Team_')].rename(columns = {"ridge_reg_value": stat}).reset_index(drop = True)
+    adjusted_df['coef_name'] = adjusted_df['coef_name'].str.replace('Team_','')
+    adjusted_df = adjusted_df.drop(columns=['ridge_reg_coef'])
+    adjusted_dict[stat] = adjusted_df
+    # print(adjusted_df.head(15))
 
-reg = linear_model.Ridge(alpha = alf, fit_intercept = True) 
+for key in adjusted_dict:
+    print(adjusted_df[key][key])
+    df['adj_'+key] = df['Team'].map(adjusted_dict[key][key])
+    print(df.head(15))
 
-reg.fit(X = dummy_df, y = df[stat])
 
-# Extract regression coefficients
-dfRegResults = pd.DataFrame({
-    'coef_name': dummy_df.columns.values,
-    'ridge_reg_coef': reg.coef_})
+# # print(dummy_df.head(15))
+# rdcv = linear_model.RidgeCV(alphas = [0.1,0.5,1,1.5,2,2.5], fit_intercept = True)
+# rdcv.fit(dummy_df,df['Raw_Def_Eff'])
+# alf = rdcv.alpha_
+# r_squared = rdcv.score(dummy_df,df['Raw_Off_Eff'])
+# print(f"\n\nridge regression r-squared: {r_squared}\n\n")
+# print(f"\n\nridge regression alpha: {alf}\n\n")
+# stat = 'Raw_Off_Eff'
 
-# Add intercept back in to reg coef to get 'adjusted' value
-dfRegResults['ridge_reg_value'] = (dfRegResults['ridge_reg_coef']+reg.intercept_)
+# reg = linear_model.Ridge(alpha = alf, fit_intercept = True) 
 
-print(dfRegResults)
+# reg.fit(X = dummy_df, y = df[stat])
+
+# # Extract regression coefficients
+# dfRegResults = pd.DataFrame({
+#     'coef_name': dummy_df.columns.values,
+#     'ridge_reg_coef': reg.coef_})
+
+# # Add intercept back in to reg coef to get 'adjusted' value
+# dfRegResults['ridge_reg_value'] = (dfRegResults['ridge_reg_coef']+reg.intercept_)
